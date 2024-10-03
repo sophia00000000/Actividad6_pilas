61e3e1df71ff69580e755545afa663d938271054
sophia00000000 <laura.hernandez10@usa.edu.co>
2024-10-03 09:38:18 -0500

    Add files via upload

---
+++ Actividad6_pilas/src/Ejer6.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+public class Ejer6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stack<Integer> pila = new Stack();
+		llenarPila(pila);
+		System.out.println("pila Invertida: ");		
+		mostrar(pila);
+
+	}
+	
+	public static void llenarPila(Stack<Integer> pila) {
+		pila.push(-1);
+		pila.push(-2);
+		pila.push(-3);
+		pila.push(-4);
+	}
+	
+	public static void mostrar(Stack<Integer> pila) {
+		
+		if (pila.isEmpty()) {
+			System.out.println("fin");
+		}else {
+			System.out.println(pila.pop());
+			mostrar(pila);
+		}
+	}
+	
+
+}
