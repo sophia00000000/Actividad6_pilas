f697541d2a9718d7b5e72d786e20f99ecffeff34
sophia00000000 <laura.hernandez10@usa.edu.co>
2024-10-03 10:13:34 -0500

    Update Ejer4.java

--- Actividad6_pilas/src/Ejer4.java
+++ Actividad6_pilas/src/Ejer4.java
@@ -1,1 +1,20 @@
+import java.util.Stack;
+
+public class Ejer4 {
+	convertirABinario(20);
+	
+	public static int convertirABinario(int n) {
+		public static Stack<Integer> resultado = new Stack();
+		if(n==0) {
+			return resultado;
+		}else {
+			residuo=(n  2);
+			n=(n%2);
+			resultado+=convertirABinario(n);
+			
+		}
+		
+	}
+
+}
 
